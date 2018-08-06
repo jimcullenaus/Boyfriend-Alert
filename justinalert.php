f372d7c8129d35cd0bbd85f56fbaa3a01076bfea
Justin DeSimpliciis <30761811+jscd@users.noreply.github.com>
2018-08-06 19:37:40 -0400

    Create justinalert.php

---
+++ justinalert.php
@@ -0,0 +1,24 @@
+<link rel="apple-touch-icon" sizes="any" href="batsig.png">
+<?php
+	$key = $_GET['key'];
+	$res = file_get_contents("http://REDACTED/beep.php?key=$key");	
+?>
+	<div id="here"></div>
+	<script>
+	var i = 0;
+	var txt = 'Alerting Justin...\nFinished!'; /* The text */
+	var speed = 100; /* The speed/duration of the effect in milliseconds */
+
+	function typeWriter() {
+		  if (i < txt.length) {
+			      document.getElementById("here").innerHTML += txt.charAt(i);
+			          i++;
+			          setTimeout(typeWriter, speed);
+		  }
+		  else{
+			window.location = "http://google.com";
+		  }
+	}
+
+	typeWriter();
+	</script>
